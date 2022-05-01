6daa019134b8de357cd36dc99eb0cc6f22889351
git Forsugar <git 945495139@qq.com>
2022-05-01 22:59:28 +0800

    lab1 change master

--- src/mr/master.go
+++ src/mr/master.go
@@ -3,6 +3,7 @@ package mr
 import (
 	"log"
 	"sync"
+	"time"
 )
 import "net"
 import "os"
@@ -10,9 +11,15 @@ import "net/rpc"
 import "net/http"
 
 type MapTaskState struct {
+	beginSecond int64
+	workId      int
+	//fileId      int
 }
 
 type ReduceTaskState struct {
+	beginSecond int64
+	workId      int
+	reduceId    int
 }
 
 type Master struct {
@@ -40,6 +47,83 @@ type Master struct {
 }
 
 // Your code here -- RPC handlers for the worker to call.
+
+func (m *Master) giveMapTask(args *MapTaskArgs, reply *MapTaskReply) error {
+	if args.workerId == -1 {
+		m.currWorkId++
+		reply.workerId = m.currWorkId
+	} else {
+		reply.workerId = args.workerId
+	}
+	log.Printf("worker %v asks for a map task\n", reply.workerId)
+
+	m.issuedMapMutex.Lock()
+	if m.mapDone {
+		m.issuedMapMutex.Unlock()
+		mapDoneProcess(reply)
+		return nil
+	}
+	if m.unIssuedMapTasks.Size() == 0 && m.issuedMapTasks.Size() == 0 {
+		m.issuedMapMutex.Unlock()
+		m.mapDone = true
+		mapDoneProcess(reply)
+		m.preparedAllReduceTasks()
+		return nil
+	}
+
+	log.Printf("%v unissued map tasks %v issued map tasks at hand\n", m.unIssuedMapTasks.Size(), m.issuedMapTasks.Size())
+	m.issuedMapMutex.Unlock() // release lock to allow unissued update
+	currTime := getNowTimeSecond()
+	ret, error := m.unIssuedMapTasks.PopBack()
+	var fileId int
+	if error != nil {
+		log.Println("no map task yet, let worker wait...")
+		fileId = -1
+	} else {
+		fileId = ret.(int)
+		reply.fileName = m.filename[fileId]
+		m.issuedMapMutex.Lock()
+		m.mapTasks[fileId].beginSecond = currTime
+		m.mapTasks[fileId].workId = reply.workerId
+		m.issuedMapTasks.Insert(fileId)
+		m.issuedMapMutex.Unlock()
+		log.Printf("giving map task %v on file %v at second %v\n", fileId, reply.fileName, currTime)
+	}
+	reply.fileId = fileId
+	reply.allDone = false
+	reply.nReduce = m.nReduce
+
+	return nil
+}
+
+func (m *Master) preparedAllReduceTasks() {
+	for i := 0; i < m.nReduce; i++ {
+		log.Printf("sending %vth file map task to channel\n", i)
+		m.unIssuedMapTasks.PutFront(i)
+	}
+}
+
+func getNowTimeSecond() int64 {
+	return time.Now().UnixNano() / int64(time.Second)
+}
+
+func mapDoneProcess(reply *MapTaskReply) {
+	log.Println("all map tasks complete, telling workers to switch to reduce mode")
+	reply.fileId = -1
+	reply.allDone = true
+}
+
+func (m *Master) joinMapTask(args *MapTaskJoinArgs, reply *MapTaskJoinReply) error {
+	return nil
+}
+
+func (m *Master) giveReduceTask(args *ReduceTaskArgs, reply *ReduceTaskReply) error {
+	return nil
+}
+
+func (m *Master) joinReduceTask(args *ReduceTaskJoinArgs, reply *ReduceTaskJoinReply) error {
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -79,6 +163,10 @@ func (m *Master) Done() bool {
 	return ret
 }
 
+func (m *Master) loopRemoveTimeOutMap() {
+
+}
+
 //
 // create a Master.
 // main/mrmaster.go calls this function.
@@ -95,7 +183,7 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m.filename = files
 	m.nReduce = nReduce
 
-	m.unIssuedReduceTasks = NewBlockQueue()
+	m.unIssuedMapTasks = NewBlockQueue()
 	m.issuedMapTasks = NewMapSet()
 
 	m.unIssuedReduceTasks = NewBlockQueue()
@@ -108,5 +196,17 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m.allDone = false
 
 	m.server()
+	log.Println("listening started...")
+
+	// starts a thread that abandons timeout tasks
+	go m.loopRemoveTimeOutMap()
+
+	// all are unissued map tasks
+	// send to channel after everything else initializes
+	log.Printf("file count %d\n", len(files))
+	for i := 0; i < len(files); i++ {
+		log.Printf("sending %vth file map task to channel\n", i)
+		m.unIssuedReduceTasks.PutFront(i)
+	}
 	return &m
 }
